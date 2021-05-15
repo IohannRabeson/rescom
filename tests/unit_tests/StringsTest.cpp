f17891b05469df48899d885b6fe573615a651dc6
irabeson <iohannrabeson@users.noreply.github.com>
2021-05-15 16:59:56 -0400

    Prevent build error with Catch2 (mixup with Catch2::trim..)

--- tests/unit_tests/StringsTest.cpp
+++ tests/unit_tests/StringsTest.cpp
@@ -2,10 +2,10 @@
 #include <catch2/catch_all.hpp>
 
 TEST_CASE("StringTests", "trim") {
-    REQUIRE( trim("") == "" );
-    REQUIRE( trim("  ") == "" );
-    REQUIRE( trim(" a b") == "a b" );
-    REQUIRE( trim("a b ") == "a b" );
-    REQUIRE( trim(" a b ") == "a b" );
+    REQUIRE( ::trim("") == "" );
+    REQUIRE( ::trim("  ") == "" );
+    REQUIRE( ::trim(" a b") == "a b" );
+    REQUIRE( ::trim("a b ") == "a b" );
+    REQUIRE( ::trim(" a b ") == "a b" );
 }
 
