fec60fd8d71005459bad147f651cdf904ef2e47d
irabeson <iohannrabeson@users.noreply.github.com>
2021-05-14 23:10:42 -0400

    Fix error `field ‘Configuration::configurationFilePath’ does not match declaration order in ‘Configuration’`

--- compiler/Configuration.cpp
+++ compiler/Configuration.cpp
@@ -65,5 +65,5 @@ std::optional<Configuration> Configuration::fromFile(std::filesystem::path const
         return left.key < right.key;
     });
 
-    return Configuration { .inputs = std::move(inputs), .configurationFilePath = configurationFilePath };
+    return Configuration { .configurationFilePath = configurationFilePath, .inputs = std::move(inputs) };
 }
