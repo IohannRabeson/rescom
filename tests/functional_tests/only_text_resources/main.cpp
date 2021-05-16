ef748af00472e5e12adab7ffeb8e3a2078f33763
irabeson <iohannrabeson@users.noreply.github.com>
2021-05-15 22:05:28 -0400

    Fix an issue with the binary search.

--- tests/functional_tests/only_text_resources/main.cpp
+++ tests/functional_tests/only_text_resources/main.cpp
@@ -4,9 +4,17 @@
 
 #include "rescom.hpp"
 
-TEST_CASE("ProjectTests", "[simple]") {
+TEST_CASE("basic", "[TextOnly]") {
     auto const& slot = rescom::getResource("test.txt");
 
     REQUIRE( std::string(slot.bytes, slot.size) == "Hello world!" );
+}
+
+TEST_CASE("invalid key", "[TextOnly]") {
+    auto const& slot = rescom::getResource("test");
+
+    REQUIRE( slot.bytes == nullptr );
+    REQUIRE( slot.size == 0 );
+    REQUIRE( slot.key == nullptr );
 }
 
